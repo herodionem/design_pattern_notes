58d811c2b9f4744510870320b0867b05baffe043
Jonathan Tieken <jonathan@oneclickretail.com>
2020-03-05 20:42:44 -0700

    adding visitor pattern

---
+++ behavioral_patterns/Visitor.py
@@ -0,0 +1,34 @@
+class House(object):
+    def accept(self, visitor):
+        visitor.visit(self)
+
+    def work_on_hvac(self, hvac_specialist):
+        print(self, f"Worked on by {hvac_specialist}")
+
+    def work_on_electricity(self, electrician):
+        print(self, f"Worked on by {electrician}")
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class Visitor(object):
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class HvacSpecialist(Visitor):
+    def visit(self, house):
+        house.work_on_hvac(self)
+
+
+class Electrician(Visitor):
+    def visit(self, house):
+        house.work_on_electricity(self)
+
+house = House()
+hvac = HvacSpecialist()
+elec = Electrician()
+
+house.accept(hvac)
+house.accept(elec)
