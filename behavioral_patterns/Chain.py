49a7c70defb7712deffa1ec2ba91d53e30594eb8
Jonathan Tieken <jonathan@oneclickretail.com>
2020-03-05 22:01:56 -0700

    augmenting chained handlers with a bit of dialog to allow user more visibility into how it's working

--- behavioral_patterns/Chain.py
+++ behavioral_patterns/Chain.py
@@ -7,18 +7,39 @@ class Handler:
         handled = self._handle(request)
 
         if not handled:
+            print(f"{self._name} can not handle request {request}. Maybe {self._successor._name} can...")
             self._successor.handle(request)
 
     def _handle(self, request):
         raise NotImplementedError("Must provide implementation in subclass!")
 
+    @property
+    def _name(*args, **kwargs):
+        return 'placeholder'
 
-class ConcreteHandler(Handler):
+
+class ConcreteHandler1(Handler):
 
     def _handle(self, request):
         if 0 < request <= 10:
-            print(f"Request {request} handled by handler 1")
+            print(f"Request {request} handled by {self._name}")
             return True
+
+    @property
+    def _name(*args, **kwargs):
+        return 'Handler 1'
+
+
+class ConcreteHandler2(Handler):
+
+    def _handle(self, request):
+        if 10 < request <= 20:
+            print(f"Request {request} handled by {self._name}")
+            return True
+
+    @property
+    def _name(*args, **kwargs):
+        return 'Handler 2'
 
 
 class DefaultHandler(Handler):
@@ -27,11 +48,19 @@ class DefaultHandler(Handler):
         print(f"End chain - no handler for request {request}")
         return True
 
+    @property
+    def _name(*args, **kwargs):
+        return 'Default Handler'
+
 
 class Client:
 
     def __init__(self):
-        self.handler = ConcreteHandler(DefaultHandler(None))
+        self.handler = ConcreteHandler1(
+                            ConcreteHandler2(
+                                DefaultHandler(None)
+                            )
+                        )
 
     def delegate(self, requests):
         for request in requests:
@@ -40,6 +69,6 @@ class Client:
 
 c = Client()
 
-requests = [2, 5, 30]
+requests = [2, 5, 19, 30]
 
 c.delegate(requests)
